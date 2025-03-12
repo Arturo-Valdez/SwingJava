176acdabfd8984c6a774a4d4a91209ece2774523
Arturo Valdez <santiguardado27@gmail.com>
2025-03-11 20:29:00 -0600

    Reaccionar a evento KeyPassed

--- src/main/java/Forma.java
+++ src/main/java/Forma.java
@@ -3,6 +3,8 @@ import com.formdev.flatlaf.FlatDarculaLaf;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Forma extends JFrame{
     private JPanel panelPrincipal;
@@ -12,15 +14,32 @@ public class Forma extends JFrame{
 
     public Forma(){
         inicializarForma();
-        campoTexto.addActionListener(e -> {
-            System.out.println("Se ejecuto action listener: " + e);
+
+        ///ActionListener nos permite que al escribir una informacion en un
+        ///cuadro de texto y al precionar enter lo muestre en un label
+//        campoTexto.addActionListener(e -> {
+//            replicarTexto();
+//        });
+
+        /// KeyAdapter nos permite que al momento de escribir informacion
+        /// Se refleje al precionar las teclas
+        campoTexto.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                super.keyTyped(e);
+                replicarTexto();
+            }
         });
+    }
+
+    private void replicarTexto() {
+        this.replicadorLabel.setText(this.campoTexto.getText());
     }
 
     private void inicializarForma(){
         setContentPane(panelPrincipal);//Lanzar contenido del panel
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// Al precionar X de la aplicacion, finaliza proceso
-        setSize(600, 300);//Medidas del panel en pixeles
+        setSize(600, 400);//Medidas del panel en pixeles
         setLocationRelativeTo(null); // Centramos ventana
     }
 
