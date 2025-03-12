13a800701ce3f1a74b8372c4afba7821cdff26ba
Arturo Valdez <santiguardado27@gmail.com>
2025-03-11 19:58:39 -0600

    Modo Oscuro

--- src/main/java/Forma.java
+++ src/main/java/Forma.java
@@ -1,3 +1,5 @@
+import com.formdev.flatlaf.FlatDarculaLaf;
+
 import javax.swing.*;
 
 public class Forma extends JFrame{
@@ -10,11 +12,12 @@ public class Forma extends JFrame{
     private void inicializarForma(){
         setContentPane(panel1);//Lanzar contenido del panel
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// Al precionar X de la aplicacion, finaliza proceso
-        setSize(1366, 768);//Medidas del panel en pixeles
+        setSize(1000, 600);//Medidas del panel en pixeles
         setLocationRelativeTo(null); // Centramos ventana
     }
 
     public static void main(String[] args) {
+        FlatDarculaLaf.setup();//Cambiar el look and feel a modo oscuro
         Forma forma = new Forma();
         forma.setVisible(true);
     }
