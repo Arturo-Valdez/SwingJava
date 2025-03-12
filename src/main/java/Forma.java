59199b2ce14288235b378a280f36149a0f76cf51
Arturo Valdez <santiguardado27@gmail.com>
2025-03-11 20:12:09 -0600

    Componente caja de Texto

--- src/main/java/Forma.java
+++ src/main/java/Forma.java
@@ -3,16 +3,19 @@ import com.formdev.flatlaf.FlatDarculaLaf;
 import javax.swing.*;
 
 public class Forma extends JFrame{
-    private JPanel panel1;
+    private JPanel panelPrincipal;
+    private JPanel panel2;
+    private JTextField campoTexto;
+    private JLabel replicadorLabel;
 
     public Forma(){
         inicializarForma();
     }
 
     private void inicializarForma(){
-        setContentPane(panel1);//Lanzar contenido del panel
+        setContentPane(panelPrincipal);//Lanzar contenido del panel
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// Al precionar X de la aplicacion, finaliza proceso
-        setSize(1000, 600);//Medidas del panel en pixeles
+        setSize(600, 300);//Medidas del panel en pixeles
         setLocationRelativeTo(null); // Centramos ventana
     }
 
